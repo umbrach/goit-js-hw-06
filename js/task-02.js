193b7c80fd7d0185ded70fb390d37fff00b7abcb
Denys Barabanov <umbrach415@gmail.com>
2022-10-04 20:28:20 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const parentEl = document.querySelector("#ingredients");
+
+function insertItem(ingredients, parentEl) {
+  const result = ingredients
+    .map((ingredient) => {
+      return `<li>${ingredient}</li>`;
+    })
+    .join("");
+
+  parentEl.insertAdjacentHTML("beforeend", result);
+}
+
+insertItem(ingredients, parentEl);
