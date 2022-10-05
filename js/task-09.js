816d621826b7fbf446cd05e2a9e326814a5a1350
Denys Barabanov <umbrach415@gmail.com>
2022-10-05 21:55:49 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+
+
+const colorChangerBtn = document.querySelector(".change-color");
+const colorValueEl = document.querySelector(".color");
+
+const changeColor = () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorValueEl.textContent = newColor;
+};
+colorChangerBtn.addEventListener("click", changeColor);
